816f52588109df0f0be165c4a525bf2f4f68f7e5
alfreerj <alfreerj@gmail.com>
2014-09-08 20:42:10 +0400

    Init test project

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<?php
+
+/**
+* @copyright	АМ (C) 2014
+*/
+
+date_default_timezone_set('Europe/Moscow');
+setlocale(LC_TIME, "ru_RU.utf8");
+?>
+<title>Тестовый проект git</title>
+</head>
+    <body>
+    <p>
+    <h1>Тестовый проект.</h1>
+    <br>
+    тестируем работу системы контроля версий git
+    <br>
+    This page uses frames. The current browser you are using does not support frames.</p>
+    <?php
+
+	?>
+    </body>
+</html>
